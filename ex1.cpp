1b8d1a07ea57ebb9bf59fd7826560d0c5b6885ff
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-05-02 15:24:12 +0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <cctype>
+
+void replaceWord(std::string& text, const std::string& oldWord, const std::string& newWord) {
+    size_t pos = text.find(oldWord);
+    while (pos != std::string::npos) {
+        text.replace(pos, oldWord.length(), newWord);
+        pos = text.find(oldWord, pos + newWord.length());
+    }
+}
+
+void capitalizeSentences(std::string& text) {
+    size_t len = text.length();
+    for (size_t i = 0; i < len; i++) {
+        if (i == 0 || (i > 0 && text[i - 1] == '.')) {
+            text[i] = std::toupper(text[i]);
+        }
+    }
+}
+
+void countLetters(const std::string& text) {
+    int letters[26] = { 0 };
+    size_t len = text.length();
+    for (size_t i = 0; i < len; i++) {
+        char c = std::tolower(text[i]);
+        if (c >= 'a' && c <= 'z') {
+            letters[c - 'a']++;
+        }
+    }
+    std::cout << "Letter counts:\n";
+    for (int i = 0; i < 26; i++) {
+        std::cout << static_cast<char>('a' + i) << ": " << letters[i] << "\n";
+    }
+}
+
+void countDigits(const std::string& text) {
+    int digits = 0;
+    size_t len = text.length();
+    for (size_t i = 0; i < len; i++) {
+        if (std::isdigit(text[i])) {
+            digits++;
+        }
+    }
+    std::cout << "Digit count: " << digits << "\n";
+}
+
+int main() {
+    std::string text;
+    std::string oldWord;
+    std::string newWord;
+
+    std::cout << "Enter some text: ";
+    std::getline(std::cin, text);
+
+    std::cout << "Enter a word to replace: ";
+    std::cin >> oldWord;
+
+    std::cout << "Enter the new word: ";
+    std::cin >> newWord;
+
+    replaceWord(text, oldWord, newWord);
+    capitalizeSentences(text);
+    countLetters(text);
+    countDigits(text);
+
+    return 0;
+}
+
---
+++ ex2.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+int countWordOccurrences(const std::string& text, const std::string& word) {
+    int count = 0;
+    std::size_t pos = 0;
+    while ((pos = text.find(word, pos)) != std::string::npos) {
+        count++;
+        pos += word.length();
+    }
+    return count;
+}
+
+int countSentences(const std::string& text) {
+    int count = 0;
+    for (char c : text) {
+        if (c == '.' || c == '!') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int countDotsAndCommas(const std::string& text) {
+    int count = 0;
+    for (char c : text) {
+        if (c == '.' || c == ',') {
+            count++;
+        }
+    }
+    return count;
+}
+
+std::string reverseText(const std::string& text) {
+    std::string reversedText = text;
+    std::reverse(reversedText.begin(), reversedText.end());
+    return reversedText;
+}
+
+std::string reverseSentences(const std::string& text) {
+    std::string reversedText = text;
+    std::size_t start = 0;
+    std::size_t end = text.find_first_of(".!");
+    while (end != std::string::npos) {
+        std::reverse(reversedText.begin() + start, reversedText.begin() + end);
+        start = end + 1;
+        end = text.find_first_of(".!", start);
+    }
+    return reversedText;
+}
+
+int main() {
+    std::string text = "This is a sample text. It contains multiple sentences. Some of them may have dots or commas.";
+    std::string word;
+    std::cout << "Enter a word to count its occurrences: ";
+    std::cin >> word;
+
+    int wordOccurrences = countWordOccurrences(text, word);
+    int sentenceCount = countSentences(text);
+    int dotAndCommaCount = countDotsAndCommas(text);
+    std::string reversedText = reverseText(text);
+    std::string reversedSentences = reverseSentences(text);
+
+    std::cout << "Number of occurrences of the word \"" << word << "\": " << wordOccurrences << std::endl;
+    std::cout << "Number of sentences in the text: " << sentenceCount << std::endl;
+    std::cout << "Number of dots and commas in the text: " << dotAndCommaCount << std::endl;
+    std::cout << "Reversed text: " << reversedText << std::endl;
+    std::cout << "Text with reversed sentences: " << reversedSentences << std::endl;
+
+    return 0;
+}
